e28a9f37ae797557c611401f0b2e03faf26a11df
leehan <leehan6005@gmail.com>
2024-11-05 09:20:28 +0900

    edit gitignore

---
+++ glucocare/lib/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- glucocare/lib/main.dart
+++ glucocare/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "package:firebase_core/firebase_core.dart" show Firebase;
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
