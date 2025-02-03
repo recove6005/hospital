a519385cb471627575c8586133f21c21a01544af
recove6005 <leehan6005@gmail.com>
2025-02-03 18:00:26 +0900

    모바일 화면 - home

---
+++ daoldraft_app/android/app/src/main/kotlin/com/aligo/aligo/daoldraft_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aligo.aligo.daoldraft_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ daoldraft_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'NanumGothic',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('HOME'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '홈화면 입니다.',
+            ),
+            Text('---', style: Theme.of(context).textTheme.headlineMedium,),
+          ],
+        ),
+      ),
+    );
+  }
+}
